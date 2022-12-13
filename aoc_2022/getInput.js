c5868204d3468313b8b614b24cf5b48059acc8d0
William Gadd <wgadd@sasktel.net>
2022-12-13 12:28:31 -0600

    Change puzzle import function to deal with both puzzles at once

--- aoc_2022/getInput.js
+++ aoc_2022/getInput.js
@@ -1,8 +1,15 @@
 const { readFile } = require('fs');
 
-const dataStringToArray = (data) => data.split('/n');
+const dataStringToArray = (data) => data.split('\n');
 
-const getPuzzleInput = (filePath, parseData, calculateResult) => {
+const getPuzzleInput = (filePath, parseData, calculateResult, printInitialArray) => {
+
+  if (!Array.isArray(parseData)) {
+    parseDataArray = [parseData]
+  }
+  else {
+    parseDataArray = parseData;
+  }
 
   readFile(filePath, { encoding: 'utf8' }, (error, data) => {
 
@@ -10,13 +17,27 @@ const getPuzzleInput = (filePath, parseData, calculateResult) => {
 
     const dataArray = dataStringToArray(data);
 
-    const startData = parseData(dataArray);
-    const result = calculateResult(startData);
+    if (printInitialArray) {
+      console.log(dataArray)
+    };
 
-    console.log(result)
+    const startData = parseDataArray[0](dataArray);
+    const result = calculateResult[0](startData);
+
+    console.log("First Solution", result)
+
+    if (calculateResult[1]) {
+      const secondStartData = parseDataArray[1] || startData;
+
+      const secondResult = calculateResult[1](secondStartData);
+
+      console.log("Second Solution", secondResult);
+
+    }
+
   })
 }
 
-module.exports = getPuzzleInput();
+module.exports = getPuzzleInput;
 
 
