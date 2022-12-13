4f355e249a63ba7bd291640b9d7eaff7d646f16a
William Gadd <wgadd@sasktel.net>
2022-12-13 11:01:16 -0600

    Convert given data into a list of the ranges as arrays

--- aoc_2022/day_4/find_overlaps.js
+++ aoc_2022/day_4/find_overlaps.js
@@ -1,10 +1,42 @@
 const { readFile } = require('fs');
 
-FILE_PATH = './example_data.txt';
+FILE_PATH = './data.txt';
+
+const getLineRanges = (line) => {
+
+  let currNumber = "";
+  let rangeArr = [];
+  const lineRanges = [];
+  for(const char of line) {
+    if (/[0-9]/.test(char)) {
+      currNumber += char;
+      continue;
+    }
+
+    rangeArr.push(parseInt(currNumber));
+    currNumber = "";
+    if (char === ',') {
+      lineRanges.push(rangeArr);
+      rangeArr = [];
+    }
+    
+  }
+
+  rangeArr.push(parseInt(currNumber));
+  lineRanges.push(rangeArr);
+
+  return lineRanges;
+
+}
 
 const parseInput = (data) => {
   console.log(data.split('\n'));
-  return data.split('\n');
+  const lines = data.split('\n');
+
+  const processedData = lines.map(line => getLineRanges(line))
+  
+  console.log(processedData);
+  return processedData;
 }
 
 readFile(FILE_PATH, { encoding: 'utf-8' }, (error, data) => {
