f02a8a34e4cc66a67057554a35cf4599bf8fd9a2
William Gadd <wgadd@sasktel.net>
2022-12-15 14:22:38 -0600

    Add functions for registering new files and new folders

--- aoc_2022/day_7/findFileDeleteCandidates.js
+++ aoc_2022/day_7/findFileDeleteCandidates.js
@@ -1,16 +1,52 @@
 const { readFile } = require("fs");
 
 const FILE_PATH = "./example_data.txt";
+/**
+ * fileObj: {
+ *  name,
+ *  size,
+ * }
+ */
+const createFile = (currentFolder, fileObj) => {
+  const newFile = { ...fileObj };
+  if (!currentFolder.files) {
+    currentFolder.files = {};
+  }
 
-const evalCommand = (command, lineObject) => {
-  const commandArray = command.split(' ');
-  const commandName = commandArray[0];
-  const path = commandArray[1] === "/" ? "root" : commandArray[1];
-  lineObject.command = commandName;
-  lineObject.path = path;
+  currentFolder.files[fileObj.name] = newFile;
 };
 
-const evalConsoleLines = (data) => {
+const createFolder = (currentFolder, folderName) => {
+  const newFolder = {
+    name: folderName,
+    folders: {
+      "..": currentFolder,
+    }
+  };
+  currentFolder.folders[foldername] = newFolder;
+};
+
+const evalCommand = (command, lineObject, currentFolder, root) => {
+  const commandArray = command.split(' ');
+  const commandName = commandArray[0];
+  if (commandName === "cd") {
+
+    const folderName = commandArray[1];
+
+    if (folderName === "/") {
+      return root;
+    }
+
+    return currentFolder.folders[folderName];
+
+  }
+  lineObject.command = commandName;
+  lineObject.path = folderName;
+
+  return currentFolder;
+};
+
+const evalConsoleLines = (data, currentFolder, root) => {
   const consoleLines = [];
   let lineObject = {};
 
@@ -22,8 +58,8 @@ const evalConsoleLines = (data) => {
 
   if (line[0] === '$') {
     lineObject.type = "command";
-    evalCommand(line.substring(2), lineObject);
-    console.log(lineObject);
+    currentFolder = evalCommand(line.substring(2), lineObject, currentFolder, root);
+    console.log(currentFolder);
   }
 
 };
@@ -37,8 +73,15 @@ dir a
 14848514 b.txt
 8504156 c.dat
 dir d
-  `;
+`;
 
+  const root = {
+    name: "root",
+    directories: {},
+    files: {}
+
+  };
   const dataArray = sampleData.split("\n");
-  evalConsoleLines(dataArray);
+  let currentFolder = root;
+  evalConsoleLines(dataArray, currentFolder, root);
 });
