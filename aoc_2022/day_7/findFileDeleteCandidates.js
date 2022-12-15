3009155acbace4a8ad4242d3de9ae8fcf7631be8
William Gadd <wgadd@sasktel.net>
2022-12-15 12:17:46 -0600

    Start advent of code day 7: delete candidates problem

---
+++ aoc_2022/day_7/findFileDeleteCandidates.js
@@ -0,0 +1,18 @@
+const { readFile } = require('fs');
+
+const FILE_PATH = "./example_data.txt";
+
+readFile(FILE_PATH, { encoding: 'utf-8'}, (error, data) => {
+  if(error) throw error;
+
+  const sampleData = (
+    `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+  `)
+
+  console.log(sampleData.split('\n'));
+})
