2f32bd559089ae24a64fdf024557eed517f4ab4d
William Gadd <wgadd@sasktel.net>
2022-12-15 12:36:15 -0600

    Add true data to aoc day_7 folder

--- aoc_2022/day_7/findFileDeleteCandidates.js
+++ aoc_2022/day_7/findFileDeleteCandidates.js
@@ -1,18 +1,17 @@
-const { readFile } = require('fs');
+const { readFile } = require("fs");
 
 const FILE_PATH = "./example_data.txt";
 
-readFile(FILE_PATH, { encoding: 'utf-8'}, (error, data) => {
-  if(error) throw error;
+readFile(FILE_PATH, { encoding: "utf-8" }, (error, data) => {
+  if (error) throw error;
 
-  const sampleData = (
-    `$ cd /
+  const sampleData = `$ cd /
 $ ls
 dir a
 14848514 b.txt
 8504156 c.dat
 dir d
-  `)
+  `;
 
-  console.log(sampleData.split('\n'));
-})
+  console.log(sampleData.split("\n"));
+});
